72d9e8974a49c670c5af1001d4c75acd746d5946
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-08 23:48:37 -0600

    Change individual grid class name

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const grid11 = document.querySelector('.grid11');
+const grid12 = document.querySelector('.grid12');
+const grid13 = document.querySelector('.grid13');
+const grid21 = document.querySelector('.grid21');
+const grid22 = document.querySelector('.grid22');
+const grid23 = document.querySelector('.grid23');
+const grid31 = document.querySelector('.grid31');
+const grid32 = document.querySelector('.grid32');
+const grid33 = document.querySelector('.grid33');
+
+console.log(grid11);
